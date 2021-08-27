c81746f948f286e3eafe5f210c4f0616fb884040
Vlad Shutchenko <vshutchenko@mail.ru>
2021-08-27 20:15:50 +0300

    Add initial version of FileCabinetApp.

---
+++ FileCabinetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetApp
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
