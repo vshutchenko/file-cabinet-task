d6ced766468fb1b786a717225aa9946ec19d813e
Vlad Shutchenko <vshutchenko@mail.ru>
2021-08-30 11:05:18 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -10,6 +10,8 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, char gender, short experience, decimal salary)
         {
+            ValidateRecordParams(firstName, lastName, dateOfBirth, gender, experience, salary);
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
@@ -34,6 +36,49 @@ namespace FileCabinetApp
         public int GetStat()
         {
             return this.list.Count;
+        }
+
+        private static void ValidateRecordParams(string firstName, string lastName, DateTime dateOfBirth, char gender, short experience, decimal salary)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName), $"{nameof(firstName)} is null or whitespace");
+            }
+
+            if ((firstName.Length < 2) || (firstName.Length > 60))
+            {
+                throw new ArgumentException($"{nameof(firstName)} length is not between 2 and 60");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName), $"{nameof(lastName)} is null or whitespace");
+            }
+
+            if ((lastName.Length < 2) || (lastName.Length > 60))
+            {
+                throw new ArgumentException($"{nameof(lastName)} length is not between 2 and 60");
+            }
+
+            if ((dateOfBirth < new DateTime(1950, 1, 1)) || (dateOfBirth > DateTime.Now))
+            {
+                throw new ArgumentOutOfRangeException(nameof(dateOfBirth), $"{nameof(dateOfBirth)} is not in range between 01-Jan-1950 and current date");
+            }
+
+            if ((gender != 'M') && (gender != 'F'))
+            {
+                throw new ArgumentException($"{nameof(gender)} is not equals to F or M");
+            }
+
+            if (experience < 0)
+            {
+                throw new ArgumentException($"{nameof(experience)} is less than zero");
+            }
+
+            if (salary < 0)
+            {
+                throw new ArgumentException($"{nameof(salary)} is less than zero");
+            }
         }
     }
 }
