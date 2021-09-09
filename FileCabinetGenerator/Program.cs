333e0f344f7da33be67ac61f6b3a45ed76d09dba
Vlad Shutchenko <vshutchenko@mail.ru>
2021-09-09 15:50:04 +0300

    Create FileCabinetGenerator project

---
+++ FileCabinetGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
